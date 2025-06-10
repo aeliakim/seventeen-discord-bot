2c09ed32c7707518bfc3da9b7c8d5858001ce1f3
Desiana Fitria <128810396+aeliakim@users.noreply.github.com>
2025-06-10 20:37:21 +0700

    feat: twitter integration with bot

--- src/twitter/index.js
+++ src/twitter/index.js
@@ -1,26 +1,64 @@
 const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config();
 
-const twitterClient = new TwitterApi({
-  appKey: process.env.TWITTER_API_KEY,
-  appSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessSecret: process.env.TWITTER_ACCESS_SECRET,
-});
+// Use Bearer Token for app-only authentication
+const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
 
 // get username
 const SEVENTEEN_USERNAME = "pledis_17";
 
 async function fetchLatestTweet() {
-  // get user ID
-  const user = await twitterClient.v2.userByUsername(SEVENTEEN_USERNAME);
-  // get latest tweets (exclude replies/retweets)
-  const tweets = await twitterClient.v2.userTimeline(user.data.id, {
-    max_results: 5,
-    exclude: "replies, retweets",
-  });
-  // return the most recent tweet
-  return tweets.data.data[0];
+  try {
+    // Get user ID
+    const user = await twitterClient.v2.userByUsername(SEVENTEEN_USERNAME);
+    // Get latest tweets (exclude replies/retweets)
+    const tweets = await twitterClient.v2.userTimeline(user.data.id, {
+      max_results: 5,
+      "tweet.fields": "created_at,attachments,entities",
+      expansions: "attachments.media_keys,author_id",
+      "media.fields": "url,preview_image_url,type",
+      exclude: "replies,retweets",
+    });
+    if (!tweets.data || !tweets.data.data || tweets.data.data.length === 0)
+      return null;
+    const tweet = tweets.data.data[0];
+    // Get media if present
+    let mediaUrl = null;
+    let mediaType = null;
+    if (
+      tweet.attachments &&
+      tweet.attachments.media_keys &&
+      tweets.data.includes &&
+      tweets.data.includes.media
+    ) {
+      const media = tweets.data.includes.media.find((m) =>
+        tweet.attachments.media_keys.includes(m.media_key)
+      );
+      if (media) {
+        mediaUrl = media.url || media.preview_image_url;
+        mediaType = media.type;
+      }
+    }
+
+    const externalUrls =
+      tweet.entities && tweet.entities.urls
+        ? tweet.entities.urls.map((url) => url.expanded_url)
+        : [];
+
+    return {
+      id: tweet.id,
+      text: tweet.text,
+      author: user.data,
+      created_at: tweet.created_at,
+      media_url: mediaUrl,
+      media_type: mediaType,
+      tweet_url: `https://twitter.com/${SEVENTEEN_USERNAME}/status/${tweet.id}`,
+      external_urls: externalUrls,
+    };
+  } catch (err) {
+    console.error("Error fetching tweet:", err);
+    return null;
+  }
 }
 
 module.exports = { fetchLatestTweet };
